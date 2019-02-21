d945932b18e7318c272b0e2cdf3a1eff46d6a245
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-02-21 16:14:48 -0300

    Move files from packages to imports

---
+++ .scripts/remove-uncessary-files.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+fs.unlinkSync('node_modules/pdfjs-dist/build/pdf.js');
+fs.unlinkSync('node_modules/pdfjs-dist/build/pdf.worker.js');
+fs.unlinkSync('node_modules/blockstack/dist/blockstack.js');
